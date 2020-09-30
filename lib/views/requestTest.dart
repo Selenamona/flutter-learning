b7082ef659123a1b8457ea63ef89199f5bb757f9
selenamona <1591541371@qq.com>
2020-09-30 16:52:16 +0800

    FutureBuilder

--- lib/views/requestTest.dart
+++ lib/views/requestTest.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import '../service/request_config.dart';
 import '../api/api.dart';
@@ -11,28 +13,41 @@ class _RequestTestState extends State<RequestTest> {
   @override
   String texts = "";
 
+  // Future fetchPost() async {
+  //   var res = await DioUtil.post(Api.getProvincePrefix);
+  //   var traceId = res;
+  // }
+
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(title: new Text("省份简称")),
-        body: Container(
-            child: Column(
-          children: [
-            GestureDetector(
-              onTap: _getProvince,
-              child: Text(
-                "获取省市",
-                style: TextStyle(
-                    decoration: TextDecoration.lineThrough,
-                    decorationColor: Colors.red,
-                    decorationStyle: TextDecorationStyle.solid), // 添加删除线
-              ),
-            ),
-            Text(
-              texts,
-              style: TextStyle(decoration: TextDecoration.none), // 去除双下划线
-            )
-          ],
-        )));
+        body: FutureBuilder(
+            // future: fetchPost(),
+            future: DefaultAssetBundle.of(context)
+                .loadString('mock/provincePrefix.json'),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              var mydata = json.decode(snapshot.data.toString());
+              print(mydata["data"]["info"]); // 获取的list
+              return Container(
+                  child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: _getProvince,
+                    child: Text(
+                      "获取省市",
+                      style: TextStyle(
+                          decoration: TextDecoration.lineThrough,
+                          decorationColor: Colors.red,
+                          decorationStyle: TextDecorationStyle.solid), // 添加删除线
+                    ),
+                  ),
+                  Text(
+                    texts,
+                    style: TextStyle(decoration: TextDecoration.none), // 去除双下划线
+                  )
+                ],
+              ));
+            }));
   }
 
   // 网络请求获取省份简称
@@ -47,3 +62,5 @@ class _RequestTestState extends State<RequestTest> {
     }
   }
 }
+
+class JSON {}
